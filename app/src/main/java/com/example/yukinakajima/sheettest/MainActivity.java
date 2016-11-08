c8cd2ff50c7960192963dafd3e662d0be69b1d4e
Yuki Nakajima <yuuki.nakajima.5v@stu.hosei.ac.jp>
2016-11-08 16:59:30 +0900

    生データの保存まで

--- app/src/main/java/com/example/yukinakajima/sheettest/MainActivity.java
+++ app/src/main/java/com/example/yukinakajima/sheettest/MainActivity.java
@@ -378,15 +378,7 @@ public class MainActivity extends Activity
             String spreadsheetId = "1KGlEF4lHTvCd5gBDcSdj3pyK7Y3lAw9bCL-_RMBxVAg";
             Time time = new Time("Asia/Tokyo");
             time.setToNow();
-            String data = (time.month + 1) + "/" + time.monthDay + " " + time.hour + ":" + time.minute + ":" + time.second;
-            String sheetName = "";
-            if (intent.hasExtra("androidaccel")) {
-                sheetName = "A/M: " + data;
-            } else if (intent.hasExtra("watch_accel_time")) {
-                sheetName = "W: " + data;
-            } else {
-                sheetName = data;
-            }
+            String sheetName = (time.month + 1) + "/" + time.monthDay + " " + time.hour + ":" + time.minute + ":" + time.second;
             BatchUpdateSpreadsheetRequest content = new BatchUpdateSpreadsheetRequest();
             List<Request> requests = new ArrayList<>();
             Request e = new Request();
@@ -406,128 +398,144 @@ public class MainActivity extends Activity
             List row = new ArrayList<>();
 
             if (intent.hasExtra("androidaccel")) {
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
                 float[] androidaccelList = intent.getFloatArrayExtra("androidaccel");
-                for (int i = 0; i < androidaccelList.length; i += 5) {
+                for (int i = 0; i < androidaccelList.length; i += 8) {
                     List col = new ArrayList<>();
                     col.add("Android ACCELEROMETER");
-                    col.add(androidaccelList[i]);
-                    col.add(androidaccelList[i + 1]);
-                    col.add(androidaccelList[i + 2]);
-                    col.add(androidaccelList[i + 3]);
-                    col.add(androidaccelList[i + 4]);
+                    col.add(androidaccelList[i]); //経過時間
+                    col.add(androidaccelList[i + 4]); //x
+                    col.add(androidaccelList[i + 5]); //y
+                    col.add(androidaccelList[i + 6]); //z
+                    col.add(androidaccelList[i + 7]); //3軸
+                    col.add("");
+                    col.add(androidaccelList[i + 1]); //x生データ
+                    col.add(androidaccelList[i + 2]); //y生データ
+                    col.add(androidaccelList[i + 3]); //z生データ
                     row.add(col);
                 }
 
-                row.add(Arrays.asList("", "", "", "", "", ""));
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
                 float[] androidgyroList = intent.getFloatArrayExtra("androidgyro");
-                for (int i = 0; i < androidgyroList.length; i += 4) {
+                for (int i = 0; i < androidgyroList.length; i += 8) {
                     List col = new ArrayList<>();
                     col.add("Android GYROSCOPE");
                     col.add(androidgyroList[i]);
+                    col.add(androidgyroList[i + 4]);
+                    col.add(androidgyroList[i + 5]);
+                    col.add(androidgyroList[i + 6]);
+                    col.add(androidgyroList[i + 7]);
+                    col.add("");
                     col.add(androidgyroList[i + 1]);
                     col.add(androidgyroList[i + 2]);
                     col.add(androidgyroList[i + 3]);
                     row.add(col);
                 }
 
-                row.add(Arrays.asList("", "", "", "", "", ""));
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
                 float[] myoaccelList = intent.getFloatArrayExtra("myoaccel");
-                for (int i = 0; i < myoaccelList.length; i += 5) {
+                for (int i = 0; i < myoaccelList.length; i += 8) {
                     List col = new ArrayList<>();
                     col.add("Myo ACCELEROMETER");
                     col.add(myoaccelList[i]);
+                    col.add(myoaccelList[i + 4]);
+                    col.add(myoaccelList[i + 5]);
+                    col.add(myoaccelList[i + 6]);
+                    col.add(myoaccelList[i + 7]);
+                    col.add("");
                     col.add(myoaccelList[i + 1]);
                     col.add(myoaccelList[i + 2]);
                     col.add(myoaccelList[i + 3]);
-                    col.add(myoaccelList[i + 4]);
                     row.add(col);
                 }
 
-                row.add(Arrays.asList("", "", "", "", "", ""));
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
                 float[] myogyroList = intent.getFloatArrayExtra("myogyro");
-                for (int i = 0; i < myogyroList.length; i += 4) {
+                for (int i = 0; i < myogyroList.length; i += 8) {
                     List col = new ArrayList<>();
                     col.add("Myo GYROSCOPE");
                     col.add(myogyroList[i]);
+                    col.add(myogyroList[i + 4]);
+                    col.add(myogyroList[i + 5]);
+                    col.add(myogyroList[i + 6]);
+                    col.add(myogyroList[i + 7]);
+                    col.add("");
                     col.add(myogyroList[i + 1]);
                     col.add(myogyroList[i + 2]);
                     col.add(myogyroList[i + 3]);
                     row.add(col);
                 }
-            } else if (intent.hasExtra("watch_accel_time")) {
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
-                float[] watch_accel_time = intent.getFloatArrayExtra("watch_accel_time");
-                float[] watch_accel_x = intent.getFloatArrayExtra("watch_accel_x");
-                float[] watch_accel_y = intent.getFloatArrayExtra("watch_accel_y");
-                float[] watch_accel_z = intent.getFloatArrayExtra("watch_accel_z");
-                float[] watch_accel_3 = intent.getFloatArrayExtra("watch_accel_3");
-                if (watch_accel_time != null) {
-                    Log.d("true", "OK");
-                    Log.d("time", Arrays.toString(watch_accel_time));
-                    Log.d("x", Arrays.toString(watch_accel_x));
-                    Log.d("y", Arrays.toString(watch_accel_y));
-                    Log.d("z", Arrays.toString(watch_accel_z));
-                    Log.d("data", Arrays.toString(watch_accel_3));
-                } else {
-                    Log.d("false", "NG");
-                }
-                for (int i = 0; i < watch_accel_time.length; i++) {
+
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
+                float[] watchaccellist = intent.getFloatArrayExtra("watchaccel");
+                for (int i = 0; i < watchaccellist.length; i += 7) {
                     List col = new ArrayList<>();
                     col.add("Watch ACCELEROMETER");
-                    col.add(watch_accel_time[i]);
-                    col.add(watch_accel_x[i]);
-                    col.add(watch_accel_y[i]);
-                    col.add(watch_accel_z[i]);
-                    float x2 = watch_accel_x[i] * watch_accel_x[i];
-                    float y2 = watch_accel_y[i] * watch_accel_y[i];
-                    float z2 = watch_accel_z[i] * watch_accel_z[i];
+                    col.add(watchaccellist[i]);
+                    col.add(watchaccellist[i + 4]);
+                    col.add(watchaccellist[i + 5]);
+                    col.add(watchaccellist[i + 6]);
+                    float x2 = watchaccellist[i + 4] * watchaccellist[i + 4];
+                    float y2 = watchaccellist[i + 5] * watchaccellist[i + 5];
+                    float z2 = watchaccellist[i + 6] * watchaccellist[i + 6];
                     float A = (float) Math.sqrt(x2 + y2 + z2);
                     col.add(A);
+                    col.add("");
+                    col.add(watchaccellist[i + 1]);
+                    col.add(watchaccellist[i + 2]);
+                    col.add(watchaccellist[i + 3]);
                     row.add(col);
                 }
 
-                row.add(Arrays.asList("", "", "", "", "", ""));
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成"));
-                float[] watch_gyro_time = intent.getFloatArrayExtra("watch_gyro_time");
-                float[] watch_gyro_x = intent.getFloatArrayExtra("watch_gyro_x");
-                float[] watch_gyro_y = intent.getFloatArrayExtra("watch_gyro_y");
-                float[] watch_gyro_z = intent.getFloatArrayExtra("watch_gyro_z");
-                for (int i = 0; i < watch_gyro_time.length; i++) {
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "x", "y", "z", "3軸合成", "", "生x", "生y", "生z"));
+                float[] watchgyrolist = intent.getFloatArrayExtra("watchgyro");
+                for (int i = 0; i < watchgyrolist.length; i += 7) {
                     List col = new ArrayList<>();
                     col.add("Watch GYROSCOPE");
-                    col.add(watch_gyro_time[i]);
-                    col.add(watch_gyro_x[i]);
-                    col.add(watch_gyro_y[i]);
-                    col.add(watch_gyro_z[i]);
+                    col.add(watchgyrolist[i]);
+                    col.add(watchgyrolist[i + 4]);
+                    col.add(watchgyrolist[i + 5]);
+                    col.add(watchgyrolist[i + 6]);
+                    float gx2 = watchgyrolist[i + 4] * watchgyrolist[i + 4];
+                    float gy2 = watchgyrolist[i + 5] * watchgyrolist[i + 5];
+                    float gz2 = watchgyrolist[i + 6] * watchgyrolist[i + 6];
+                    float AG = (float) Math.sqrt(gx2 + gy2 + gz2);
+                    col.add(AG);
+                    col.add("");
+                    col.add(watchgyrolist[i + 1]);
+                    col.add(watchgyrolist[i + 2]);
+                    col.add(watchgyrolist[i + 3]);
                     row.add(col);
                 }
 
-                row.add(Arrays.asList("", "", "", "", "", ""));
-                row.add(Arrays.asList("SensorType", "経過時間(ms)", "value"));
-                float[] watch_press_time = intent.getFloatArrayExtra("watch_press_time");
-                float[] watch_press_value = intent.getFloatArrayExtra("watch_press_value");
-                for (int i = 0; i < watch_press_time.length; i++) {
+                row.add(Arrays.asList("", "", "", "", "", "", "", "", "", ""));
+                row.add(Arrays.asList("SensorType", "経過時間(ms)", "value", "", "生value"));
+                float[] watchpresslist = intent.getFloatArrayExtra("watchpress");
+                for (int i = 0; i < watchpresslist.length; i += 3) {
                     List col = new ArrayList<>();
                     col.add("Watch PRESSURE");
-                    col.add(watch_press_time[i]);
-                    col.add(watch_press_value[i]);
+                    col.add(watchpresslist[i]);
+                    col.add(watchpresslist[i+2]);
+                    col.add("");
+                    col.add(watchpresslist[i+1]);
                     row.add(col);
                 }
-            } else {
-                row.add(Arrays.asList("There", "is", "no", "intent", "..."));
+
             }
 
             valueRange.setValues(row);
-            String range = sheetName + "!A1:F" + row.size();
+            String range = sheetName + "!A1:J" + row.size();
             valueRange.setRange(range);
-            this.mService.spreadsheets().values()
-                    .update(spreadsheetId, range, valueRange)
-                    .setValueInputOption("USER_ENTERED")
-                    .execute();
+            this.mService.spreadsheets().
+                    values().
+                    update(spreadsheetId, range, valueRange).
+                    setValueInputOption("USER_ENTERED").
+                    execute();
         }
 
         /**
